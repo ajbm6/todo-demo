25ee76ec7e13257fdac38f00caca97dfac78a206
Hugo Hamon <hugo.hamon@sensiolabs.com>
2013-12-20 16:54:43 -0500

    [DependencyInjection] refactored the container creation

--- index.php
+++ index.php
@@ -4,22 +4,15 @@ require_once realpath(__DIR__.'/vendor/autoload.php');
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\Config\FileLocator;
 use Todo\ApplicationKernel;
 
 $container = new ContainerBuilder();
 $container->setParameter('kernel.root_dir', __DIR__);
-$container->setParameter('file_locator.paths', array('%kernel.root_dir%/config'));
-
-$loader = new YamlFileLoader($container, new FileLocator(__DIR__));
-$loader->load('config/services.yml');
-
 
 $request = Request::createFromGlobals();
 $request->overrideGlobals();
 
-$kernel = new ApplicationKernel($container->get('http_kernel'));
+$kernel = new ApplicationKernel($container);
 $response = $kernel->handle($request);
 $response->prepare($request);
 $response->send();
--- src/Todo/ApplicationKernel.php
+++ src/Todo/ApplicationKernel.php
@@ -2,20 +2,27 @@
 
 namespace Todo;
 
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Config\FileLocator;
 
 class ApplicationKernel implements HttpKernelInterface
 {
-    private $httpKernel;
+    private $container;
 
-    public function __construct(HttpKernelInterface $httpKernel)
+    public function __construct(ContainerInterface $container)
     {
-        $this->httpKernel = $httpKernel;
+        $this->container = $container;
     }
 
     public function handle(Request $request, $type = self::MASTER_REQUEST, $catch = true)
     {
-        return $this->httpKernel->handle($request, $type, $catch);
+        $root = $this->container->getParameter('kernel.root_dir');
+        $loader = new YamlFileLoader($this->container, new FileLocator($root));
+        $loader->load('config/services.yml');
+
+        return $this->container->get('http_kernel')->handle($request, $type, $catch);
     }
 } 
